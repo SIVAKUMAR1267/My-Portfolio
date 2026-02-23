a21e13c953db8fd293c97de197aa15f030c3f3e5
SIVAKUMAR1267 <sivakumar.r.6324@gmail.com>
2026-02-23 17:21:19 +0530

    feat: update deployment pipeline and ESLint configuration for improved project structure

--- sivakumar-portfolio/eslint.config.js
+++ sivakumar-portfolio/eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist','playwright.config.js']),
+  globalIgnores(['dist','playwright.config.js','**/node_modules/**','**/test-results/**','**/playwright-report/**']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
